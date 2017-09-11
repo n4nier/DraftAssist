573a326ba45c2951e2cdadd94f4f45b4d7c8ac80
Nick Fournier <nick.fournier@avssystems.ca>
2017-09-11 10:19:41 -0700

    Initial Commit

---
+++ Draft Assist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Draft Assist
+//
+//  Created by Nick Fournier on 2017-09-11.
+//  Copyright © 2017 Nick Fournier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
