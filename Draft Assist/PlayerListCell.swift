33542ef5b9e7a8cd46306fc0b5b4bb63e3cfbbf6
Nick Fournier <nick.fournier@avssystems.ca>
2017-09-11 13:31:23 -0700

    first session

---
+++ Draft Assist/PlayerListCell.swift
@@ -0,0 +1,16 @@
+//
+//  PlayerListCell.swift
+//  Draft Assist
+//
+//  Created by Nick Fournier on 2017-09-11.
+//  Copyright © 2017 Nick Fournier. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PlayerListCell: UITableViewCell {
+    
+    @IBOutlet weak var PlayerName: UILabel!
+    
+}
--- Draft Assist/ViewController.swift
+++ Draft Assist/ViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var PlayerList: UITableView!
+    
+    var playerArray: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        PlayerList.delegate = self
+        PlayerList.dataSource = self
+        
+        playerArray = ["John", "Andy", "Dale"]
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! PlayerListCell
+        cell.PlayerName.text = playerArray[indexPath.row]
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return playerArray.count
+    }
 
 }
 
