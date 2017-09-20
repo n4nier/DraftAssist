b4b6cdeb15c613784d5190faab8d4457d76ba8b6
Nick Fournier <nick.fournier@avssystems.ca>
2017-09-20 16:38:31 -0700

    Session 4: DraftSetup

--- Draft Assist/DraftSetup.swift
+++ Draft Assist/DraftSetup.swift
@@ -8,9 +8,53 @@
 
 import UIKit
 
-class DraftSetup: UIViewController {
+class DraftSetup: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var RoundList: UITableView!
+    @IBOutlet weak var PlayerList: UITableView!
+    
+    var playerArray: [String] = []
+    var roundArray: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        PlayerList.delegate = self
+        PlayerList.dataSource = self
+        
+        roundArray = []
+        playerArray = ["John", "Andy", "Dale"]
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var Count: Int?
+        
+        if tableView == self.RoundList {
+            Count = roundArray.count
+        }
+        
+        if tableView == self.PlayerList {
+            Count = playerArray.count
+        }
+        
+        return Count!
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        if tableView == self.RoundList {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "roundCell") as! PlayerListCell
+            cell.PlayerName.text = roundArray[indexPath.row]
+            return cell
+        }
+        else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "playerCell") as! PlayerListCell
+            cell.PlayerName.text = playerArray[indexPath.row]
+            return cell
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 }
---
+++ Draft Assist/FullPlayerListCell.swift
@@ -0,0 +1,16 @@
+//
+//  FullPlayerListCell.swift
+//  Draft Assist
+//
+//  Created by Nick Fournier on 2017-09-20.
+//  Copyright © 2017 Nick Fournier. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FullPlayerListCell: UITableViewCell {
+    
+    @IBOutlet weak var PlayerName: UILabel!
+    
+}
